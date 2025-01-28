2d561ea7d63178ade5c89c4285022fabb2cc578d
Ömer KÜÇÜK <omer_kck@msn.com>
2025-01-29 00:49:46 +0300

    upt.

--- Core.Security/Hashing/HashingService.cs
+++ Core.Security/Hashing/HashingService.cs
@@ -5,19 +5,19 @@ namespace Core.Security.Hashing;
 
 public static class HashingService
 {
-    public static async Task<(string PasswordHash, string PasswordSalt)> CreatePasswordHashAsync(string password)
+    public static async Task<(byte[] PasswordHash, byte[] PasswordSalt)> CreatePasswordHashAsync(string password)
     {
         using var hmac = new HMACSHA512();
-        var passwordSalt = Convert.ToBase64String(hmac.Key); // Salt'ı base64 formatında döndür
-        var passwordHash = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(password))); // Hash'i base64 formatında döndür
+        var passwordSalt = hmac.Key; // Salt'ı base64 formatında döndür
+        var passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)); // Hash'i base64 formatında döndür
 
         return await Task.FromResult((passwordHash, passwordSalt));
     }
 
-    public static async Task<bool> VerifyPasswordHashAsync(string password, string passwordHash, string passwordSalt)
+    public static async Task<bool> VerifyPasswordHashAsync(string password, byte[] passwordHash, byte[] passwordSalt)
     {
-        var saltBytes = Convert.FromBase64String(passwordSalt); // Salt'ı byte dizisine çevir
-        var storedHashBytes = Convert.FromBase64String(passwordHash); // Hash'i byte dizisine çevir
+        var saltBytes = passwordSalt; // Salt'ı byte dizisine çevir
+        var storedHashBytes = passwordHash; // Hash'i byte dizisine çevir
 
         using var hmac = new HMACSHA512(saltBytes);
         var computedHashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)); // Yeni hash'i hesapla
