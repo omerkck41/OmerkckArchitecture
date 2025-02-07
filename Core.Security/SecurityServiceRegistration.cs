30162b80ce1671e7cfcb6485ef7a6c25ab3747c9
Ömer KÜÇÜK <omer_kck@msn.com>
2025-02-07 22:10:34 +0300

    SecurityServiceRegistration has been added.

---
+++ Core.Security/SecurityServiceRegistration.cs
@@ -0,0 +1,31 @@
+﻿using Core.Security.EmailAuthenticator;
+using Core.Security.MFA;
+using Core.Security.OAuth;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Security;
+
+public static class SecurityServiceRegistration
+{
+    public static IServiceCollection AddSecurityServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // MFA Servisleri
+        services.AddSingleton<TotpService>();
+        services.AddSingleton<IMfaService, MfaService>();
+
+        // EmailAuthenticatorHelper
+        services.AddSingleton<IEmailAuthenticatorHelper, EmailAuthenticatorHelper>();
+
+
+        // OAuth Servisleri
+        // OAuth yapılandırmasını appsettings.json'dan yükle
+        var oauthConfig = configuration.GetSection("OAuthSettings").Get<OAuthConfiguration>();
+        // HttpClient ve OAuthService'i dependency injection'a ekle
+        services.AddHttpClient();
+        services.AddSingleton(oauthConfig);
+        services.AddScoped<OAuthService>();
+
+        return services;
+    }
+}
