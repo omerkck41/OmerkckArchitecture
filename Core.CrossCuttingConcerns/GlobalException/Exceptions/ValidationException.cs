0edc0fdc5ffb4adac872849e6c618f01db90ad1f
Ömer KÜÇÜK <omer_kck@msn.com>
2025-02-16 22:47:21 +0300

    upt.

--- Core.CrossCuttingConcerns/GlobalException/Exceptions/ValidationException.cs
+++ Core.CrossCuttingConcerns/GlobalException/Exceptions/ValidationException.cs
@@ -1,12 +1,22 @@
-﻿namespace Core.CrossCuttingConcerns.GlobalException.Exceptions;
+﻿using System.Text.Json;
+
+namespace Core.CrossCuttingConcerns.GlobalException.Exceptions;
 
 public class ValidationException : CustomException
 {
     public Dictionary<string, string[]> Errors { get; }
 
     public ValidationException(Dictionary<string, string[]> errors)
-        : base("One or more validation failures have occurred.", errors)
+        : base(CreateErrorMessage(errors))
     {
         Errors = errors ?? new Dictionary<string, string[]>();
+    }
+
+    private static string CreateErrorMessage(Dictionary<string, string[]> errors)
+    {
+        return JsonSerializer.Serialize(errors, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
     }
 }
