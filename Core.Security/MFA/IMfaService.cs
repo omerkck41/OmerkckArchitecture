0b552426760bedba61f2eb8ee96de4dde748bc3f
Ömer KÜÇÜK <omer_kck@msn.com>
2025-02-20 21:51:16 +0300

    Otp Service restructured

--- Core.Security/MFA/IMfaService.cs
+++ Core.Security/MFA/IMfaService.cs
@@ -5,24 +5,26 @@ public interface IMfaService
     // Authenticator (6 haneli rastgele kod) üretir.
     Task<string> GenerateAuthenticatorCodeAsync();
 
+
     // Authenticator kodunu doğrular.
     Task<bool> ValidateAuthenticatorCodeAsync(string inputCode, string expectedCode);
 
+    // TOTP (Time-Based One-Time Password) (6 haneli SecretKey) üretir.
+    Task<string> GenerateOtpSecretKey();
     // TOTP (Time-Based One-Time Password) üretir.
     Task<string> GenerateOtpCodeAsync(string secretKey);
-
     // TOTP kodunu doğrular.
     Task<bool> ValidateOtpCodeAsync(string inputCode, string secretKey);
 
+
     // E-posta ile gönderilecek 6 haneli rastgele kod üretir.
     Task<string> GenerateEmailCodeAsync();
-
     // E-posta kodunu doğrular.
     Task<bool> ValidateEmailCodeAsync(string inputCode, string expectedCode);
 
+
     // SMS ile gönderilecek 6 haneli rastgele kod üretir.
     Task<string> GenerateSmsCodeAsync();
-
     // SMS kodunu doğrular.
     Task<bool> ValidateSmsCodeAsync(string inputCode, string expectedCode);
 }
--- Core.Security/MFA/MfaService.cs
+++ Core.Security/MFA/MfaService.cs
@@ -13,25 +13,27 @@ public class MfaService(IOtpService totpService) : IMfaService
 
     // 6 haneli rastgele Authenticator kodu üretir.
     public async Task<string> GenerateAuthenticatorCodeAsync() => await GenerateRandomCodeAsync();
-
     // Authenticator kodunu doğrular.
     public async Task<bool> ValidateAuthenticatorCodeAsync(string inputCode, string expectedCode) => await Task.FromResult(inputCode == expectedCode);
 
+
+    //TOTP SecretKey
+    public async Task<string> GenerateOtpSecretKey() => await _totpService.GenerateSecretKey();
     // TOTP kodu üretir.
     public async Task<string> GenerateOtpCodeAsync(string secretKey) => await _totpService.GenerateOtpCodeAsync(secretKey);
-
     // TOTP kodunu doğrular.
     public async Task<bool> ValidateOtpCodeAsync(string inputCode, string secretKey) => await _totpService.ValidateOtpCodeAsync(inputCode, secretKey);
 
+
+
     // 6 haneli rastgele e-posta kodu üretir.
     public async Task<string> GenerateEmailCodeAsync() => await GenerateRandomCodeAsync();
-
     // E-posta kodunu doğrular.
     public async Task<bool> ValidateEmailCodeAsync(string inputCode, string expectedCode) => await Task.FromResult(inputCode == expectedCode);
 
+
     // 6 haneli rastgele SMS kodu üretir.
     public async Task<string> GenerateSmsCodeAsync() => await GenerateRandomCodeAsync();
-
     // SMS kodunu doğrular.
     public async Task<bool> ValidateSmsCodeAsync(string inputCode, string expectedCode) => await Task.FromResult(inputCode == expectedCode);
 }
