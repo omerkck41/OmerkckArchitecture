032e2420b66e641d0c8add8e2982491accb3b710
Ömer KÜÇÜK <omer_kck@msn.com>
2025-03-02 01:46:42 +0300

    updated.

--- Core.Application/Authorization/Models/GeneralOperationClaims.cs
+++ Core.Application/Authorization/Models/GeneralOperationClaims.cs
@@ -8,4 +8,12 @@ public static class GeneralOperationClaims
 
     // Add more roles dynamically if needed
     public static readonly List<string> AllRoles = [Admin, Manager, User];
+
+
+    public const string ViewUsers = "ViewUsers";
+    public const string ManageUsers = "ManageUsers";
+    public const string ViewOperationClaims = "ViewOperationClaims";
+    public const string ManageOperationClaims = "ManageOperationClaims";
+    public const string ViewData = "ViewData";
+    public const string ManageData = "ManageData";
 }
