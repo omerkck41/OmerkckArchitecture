8c910a1a47971d658c26f5eeebe8f0705914be9e
Ömer KÜÇÜK <omer_kck@msn.com>
2025-03-18 02:00:58 +0300

    updated.

--- Core.Application/Authorization/Middleware/HttpAuthorizationMiddleware.cs
+++ Core.Application/Authorization/Middleware/HttpAuthorizationMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 
 namespace Core.Application.Authorization.Middleware;
 
@@ -13,6 +14,15 @@ public class HttpAuthorizationMiddleware
 
     public async Task InvokeAsync(HttpContext context)
     {
+        // Endpoint metadata bilgisini alıyoruz
+        var endpoint = context.GetEndpoint();
+        if (endpoint != null && endpoint.Metadata.Any(m => m is IAllowAnonymous))
+        {
+            // Eğer endpoint [AllowAnonymous] içeriyorsa, kontrolü atla
+            await _next(context);
+            return;
+        }
+
         if (context.User == null || !context.User.Identity!.IsAuthenticated)
         {
             context.Response.StatusCode = 401; // Unauthorized
