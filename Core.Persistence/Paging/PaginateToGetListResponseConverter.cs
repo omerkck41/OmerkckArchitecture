b89afae0e8270644c5621772a81356f02de7bc53
Ömer KÜÇÜK <omer_kck@msn.com>
2025-03-19 01:52:03 +0300

    updated.

---
+++ Core.Persistence/Paging/PaginateToGetListResponseConverter.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+
+namespace Core.Persistence.Paging;
+
+public class PaginateToGetListResponseConverter<TSource, TDestination> : ITypeConverter<IPaginate<TSource>, GetListResponse<TDestination>>
+{
+    public GetListResponse<TDestination> Convert(IPaginate<TSource> source, GetListResponse<TDestination> destination, ResolutionContext context)
+    {
+        // İlgili elemanları dönüştürmek için AutoMapper'ı kullanıyoruz
+        var mappedItems = context.Mapper.Map<IList<TDestination>>(source.Items);
+
+        var response = new GetListResponse<TDestination>
+        {
+            Items = mappedItems,
+            MetaData = new PageMetaData
+            {
+                Index = source.Index,
+                Size = source.Size,
+                TotalRecords = source.TotalRecords,
+                Pages = source.Pages
+            }
+        };
+        return response;
+    }
+}
