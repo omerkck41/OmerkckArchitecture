08d138d8781f9df673856083b6fdf079b0bcdeef
Ömer KÜÇÜK <omer_kck@msn.com>
2025-03-19 02:21:40 +0300

    updated.

--- Core.Persistence/Paging/Paginate.cs
+++ Core.Persistence/Paging/Paginate.cs
@@ -40,9 +40,8 @@ public class Paginate<T> : IPaginate<T>
     public int Count { get; set; }
     public int Pages { get; set; }
     public IList<T> Items { get; set; }
-    public bool HasPrevious => Index - From > 0;
-    public bool HasNext => Index - From < Pages - 1;
-    public int TotalRecords { get; set; }
+    public bool HasPrevious => Index > 0;
+    public bool HasNext => Index < Pages - 1;
     public bool IsFirstPage => Index == 0;
     public bool IsLastPage => Index == Pages - 1;
 }
