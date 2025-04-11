67da6215ac06bec476e5ea97b4878507415ed748
Ömer KÜÇÜK <omer_kck@msn.com>
2025-04-11 17:41:31 +0300

    updated

--- Core.CrossCuttingConcerns/GlobalException/Middlewares/GlobalExceptionMiddleware.cs
+++ Core.CrossCuttingConcerns/GlobalException/Middlewares/GlobalExceptionMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Core.CrossCuttingConcerns.GlobalException.Handlers;
+using Microsoft.Extensions.Options;
 
 namespace Core.CrossCuttingConcerns.GlobalException.Middlewares;
 
@@ -8,11 +9,11 @@ public class GlobalExceptionMiddleware
     private readonly IExceptionHandlerFactory _handlerFactory;
     private readonly GlobalExceptionOptions _options;
 
-    public GlobalExceptionMiddleware(RequestDelegate next, IExceptionHandlerFactory handlerFactory, GlobalExceptionOptions options)
+    public GlobalExceptionMiddleware(RequestDelegate next, IExceptionHandlerFactory handlerFactory, IOptions<GlobalExceptionOptions> options)
     {
         _next = next;
         _handlerFactory = handlerFactory;
-        _options = options;
+        _options = options.Value;
     }
 
     public async Task InvokeAsync(HttpContext context)
