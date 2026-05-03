ed89a0c0aae914c1a86096e00edd275af32ff347
Ömer KÜÇÜK <omer_kck@msn.com>
2026-05-03 23:17:22 +0300

    feat(hosting): add Kck.Hosting.Aspire with AddKckServiceDefaults

---
+++ tests/Kck.Hosting.Aspire.Tests/AspireExtensionsTests.cs
@@ -0,0 +1,44 @@
+using FluentAssertions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Kck.Hosting.Aspire.Tests;
+
+public sealed class AspireExtensionsTests
+{
+    [Fact]
+    public void AddKckServiceDefaults_ShouldRegisterHealthChecks()
+    {
+        var builder = Host.CreateApplicationBuilder();
+
+        builder.AddKckServiceDefaults();
+
+        var app = builder.Build();
+        app.Services.GetService(typeof(HealthCheckService)).Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddKckServiceDefaults_CalledTwice_ShouldNotThrow()
+    {
+        var builder = Host.CreateApplicationBuilder();
+
+        var act = () =>
+        {
+            builder.AddKckServiceDefaults();
+            builder.AddKckServiceDefaults();
+        };
+
+        act.Should().NotThrow();
+    }
+
+    [Fact]
+    public void AddKckServiceDefaults_NullBuilder_ShouldThrowArgumentNull()
+    {
+        IHostApplicationBuilder builder = null!;
+
+        var act = () => builder.AddKckServiceDefaults();
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+}
